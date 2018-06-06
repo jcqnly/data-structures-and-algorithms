0f67f664959c4f64f4e038259811608bebbc2d27
jcqnly <jcqnly@gmail.com>
2018-06-06 09:40:06 -0700

    fix code

--- Challenges/array_reverse/array_reverse/Program.cs
+++ Challenges/array_reverse/array_reverse/Program.cs
@@ -5,21 +5,29 @@ namespace array_reverse
     class Program
     {
         static void Main(string[] args)
-        {
-            ArrayReverse();
+        {   //hard code a given array to pass to method to reverse
+            int[] givenArray = new int[] { 3, 2, 1 };
+            ReverseArray(givenArray);
+            Console.Read();
         }
 
-        public static void ArrayReverse()
+        static int[] ReverseArray(int[] arrayToReverse)
         {
-            int[] GivenArray = new int[] { 3, 2, 1 };
-            int[] ReverseArray = new int[GivenArray.Length];
-
-            for (var i = GivenArray.Length - 1; i >=0; i--)
-            {
-                ReverseArray[i] = GivenArray[i];
-                Console.WriteLine(ReverseArray[i]);
+            int counter = 0;
+            //set new array to the length of givenArray
+            int[] reverseArray = new int[arrayToReverse.Length];
+            //working from the end of givenArray
+            for (int i = arrayToReverse.Length - 1; i >= 0; i--)
+            {   //add that value to the beginning of the new array
+                reverseArray[counter] = arrayToReverse[i];
+                counter++;
             }
+            //print for show
+            foreach (var item in reverseArray)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            return reverseArray;
         }
-
     }
 }
