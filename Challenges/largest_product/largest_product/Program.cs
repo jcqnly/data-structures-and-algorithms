72fc6cea6beed869d03c5ae7f835fe8745bc6ddd
jcqnly <jcqnly@gmail.com>
2018-06-07 10:35:35 -0700

    add project base

---
+++ Challenges/largest_product/largest_product/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace largest_product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] givenArray = new int[,] { { 1, 2, 1 }, { 2, 4, 2 }, { 3, 6, 8 }, { 7, 8, 1 } };
+
+            AdjacentProduct(givenArray);
+            Console.Read();
+        }
+
+        static void AdjacentProduct(int[,] givenArray)
+        {
+            for (var i = 0; i < givenArray.Length - 1; i++)
+            {
+                Console.WriteLine(givenArray[i, i]);
+            }
+        }
+    }
+}
