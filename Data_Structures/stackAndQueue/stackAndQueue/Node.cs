0d26407158be94ca70a7d62b00ac8235876d40ae
jcqnly <jcqnly@gmail.com>
2018-06-18 11:28:43 -0700

    stack finish

--- Data_Structures/stackAndQueue/stackAndQueue/Node.cs
+++ Data_Structures/stackAndQueue/stackAndQueue/Node.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace stackAndQueue
 {
-    class Node
+    public class Node
     {
         public int Value { get; set; }
         public Node Next { get; set; }
@@ -12,6 +12,7 @@ namespace stackAndQueue
         public Node(int value)
         {
             Value = value;
+            Next = null;
         }
     }
 }
--- Data_Structures/stackAndQueue/stackAndQueue/Program.cs
+++ Data_Structures/stackAndQueue/stackAndQueue/Program.cs
@@ -8,16 +8,28 @@ namespace stackAndQueue
         {
             Console.WriteLine("Hello World!");
 
-            //add a few nodes
-            //output a few nodes
-            
-            //instantiate stack
+            //instantiate stack and push(add) a few nodes
+            Stack stack = new Stack(new Node(9));
+            stack.Push(new Node(4));
+            stack.Push(new Node(41));
+            stack.Push(new Node(1));
 
+            //output will be (top) 1   41   4   9
+            stack.Print();
+
+            //Popping a node off the stack
+            Console.WriteLine("The popped node is: ");
+            Node poppedNode = stack.Pop();
+            Console.WriteLine(poppedNode.Value);
+
+
+            //Peeking at the top node of the stack
+            Console.WriteLine("The peeked(top) node is: ");
+            Node peekedNode = stack.Peek();
+            Console.WriteLine(peekedNode.Value);
+
+           /* 
             //instantiate queue
-
-           /* Pushing a node onto your stack
-            Popping a node off your stack
-            Peeking at the top node of your stack
             Enqueue a node into your queue
             Dequeue a node off of your queue
             Peek at the front of your queue.*/
