024a3b40a39ac2faf0cf5f252cacb09ab5f9fa6f
jcqnly <jcqnly@gmail.com>
2018-06-18 21:17:40 -0700

    finish unit testing

--- Data_Structures/stackAndQueue/stackAndQueue/Node.cs
+++ Data_Structures/stackAndQueue/stackAndQueue/Node.cs
@@ -6,9 +6,11 @@ namespace stackAndQueue
 {
     public class Node
     {
+        //establish the properties of the node
         public int Value { get; set; }
         public Node Next { get; set; }
-
+        
+        //requirements for the instantiation of a node
         public Node(int value)
         {
             Value = value;
