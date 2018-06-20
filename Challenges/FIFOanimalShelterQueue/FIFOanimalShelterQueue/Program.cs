1c207355b229d0ecb909a1277b807810c39d4cef
jcqnly <jcqnly@gmail.com>
2018-06-20 11:31:38 -0700

    solution for animal shelter Queue challenge

---
+++ Challenges/FIFOanimalShelterQueue/FIFOanimalShelterQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FIFOanimalShelterQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("G'day! Welcome to the shelter");
+            FIFOanimalShelter();
+        }
+
+        public static void FIFOanimalShelter()
+        {
+            //create a queue of animals to work with
+            Queue queue = new Queue(new Node("cat"));
+            queue.Enqueue(new Node("cat"));
+            queue.Enqueue(new Node("cat"));
+            queue.Enqueue(new Node("dog"));
+            queue.Enqueue(new Node("dog"));
+
+            //output will be  cat  cat  cat  dog  dog
+            Console.WriteLine("Animal queue we're working with: ");
+            queue.Print();
+
+            //Enqueue another animal to the queue
+            queue.Enqueue(new Node("dog"));
+            Console.WriteLine("Let's add another dog to the queue: ");
+            queue.Print();
+
+            //Dequeue an animal based on preference of the adopter
+            //accounting for when they want something other than a dog or cat
+            Console.WriteLine("Someone wants to adopt a PARROT today!");
+            //call dequeue on the queue...
+            Node chosenAnimal = queue.Dequeue("parrot");
+            Console.WriteLine($"but they're gonna get a: {chosenAnimal.AnimalType}");
+
+            Console.WriteLine("Next person wants a DOG: ");
+            Node chosenAnimalDog = queue.Dequeue("dog");
+
+            Console.WriteLine("Next person wants a CAT: ");
+            Node chosenAnimalCat = queue.Dequeue("cat");
+            queue.Print();
+        }
+    }
+}
---
+++ Challenges/FIFOanimalShelterQueue/FIFOanimalShelterQueue/Stacks.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOanimalShelterQueue
+{
+    public class Stacks
+    {
+        //estalishing the properties
+        public Node Top { get; set; }
+
+        //set the value of the Top
+        public Stacks(Node node)
+        {
+            Top = node;
+        }
+
+        /// <summary>
+        /// Push, add, a node to the top.  This takes O(1) time.
+        /// </summary>
+        /// <param name="node"></param>
+        public void Push(Node node)
+        {
+            //assign the next property of node, to be added, to point to the top
+            node.Next = Top;
+            //reassign the value of top to the newly added node
+            Top = node;
+        }
+
+        /// <summary>
+        /// Pop, remove, a node from the top.  This takes O(1) time.
+        /// the Top node will be reassigned to the node below the current top
+        /// </summary>
+        public Node Pop()
+        {
+            //create a reference that points to the same top node
+            Node temp = Top;
+
+            //reassign the value of Top to the value Next is pointing to
+            Top = Top.Next;
+
+            //set the Next pointer of temp to point to null
+            temp.Next = null;
+            return temp;
+        }
+
+        /// <summary>
+        /// view the top node of the stack, which takes O(1) time
+        /// It's best to peek before popping, otherwise a NullException
+        /// error will be thrown
+        /// </summary>
+        public Node Peek()
+        {
+            return Top;
+        }
+
+        /// <summary>
+        /// Print out all values in the list by iterating through until null is reached
+        /// This will take O(N)
+        /// </summary>
+        /// <param name="node"></param>
+        public void Print()
+        {
+            //establish a Node called current to help iterate through the nodes
+            //without resetting what the Top value is for other methods
+            Node current = Top;
+            Console.WriteLine("Printing out: \n");
+            //keep looping if the next value doesn't point to null
+            while (current.Next != null)
+            {   //if it doesn't point to null, print out the value
+                Console.Write($"  {current.AnimalType}  ");
+                //set the current value to the next value to repeat the process
+                current = current.Next;
+            }
+            Console.WriteLine($"  {current.AnimalType}  ");
+            Console.WriteLine("\n");
+        }
+    }
+}
