75e9af867ffd02783f75227fd697168262fb0a2e
jcqnly <jcqnly@gmail.com>
2018-06-27 10:14:15 -0700

    add 3 tests assertions

--- Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -26,45 +26,11 @@ namespace FizzBuzzTree
             nodeC.LeftChild = nodeF;
 
             //call the method that will test the tree's fizzbuzzyness
-            FizzBuzzTree(nodeA);
+            binaryTree.FBTree(nodeA);
 
             Console.WriteLine("Printing the new tree via the preorder traversal : ");
             binaryTree.PreOrder(nodeA);
         }
-
-        /// <summary>
-        /// traverses the tree with using a preorder route and
-        /// determines if a tree's fizzbuzzyness
-        /// </summary>
-        /// <param name="node"></param>
-        /// <returns>node</returns>
-        public static Node FizzBuzzTree(Node node)
-        {
-            Console.WriteLine(node.Value);
-            //convert the node's value to an int
-            Int32.TryParse(node.Value, out int valueOfNode);
-            if (valueOfNode % 5 == 0 && valueOfNode % 3 == 0)
-            {   //test for the strictest condition first
-                node.Value = "FizzBuzz";
-            } else if (valueOfNode%3 == 0)
-            {   //then test if the int is divisible by 3
-                node.Value = "Fizz";
-            } else if (valueOfNode%5 == 0)
-            {   //then test if the int is divisible by 5
-                node.Value = "Buzz";
-            }
-            if (node.LeftChild != null)
-            {   //keep calling the method to continue traversing
-                //the tree if there are still nodes on either
-                //side
-                FizzBuzzTree(node.LeftChild);
-            }
-
-            if (node.RightChild != null)
-            {
-                FizzBuzzTree(node.RightChild);
-            }
-            return node;
-        }
+        
     }
 }
--- Challenges/FizzBuzzTree/FizzBuzzTreeTest/TestFizzBuzzTree.cs
+++ Challenges/FizzBuzzTree/FizzBuzzTreeTest/TestFizzBuzzTree.cs
@@ -1,18 +1,20 @@
 using FizzBuzzTree;
-using System;
 using Xunit;
 
 namespace FizzBuzzTreeTest
 {
     public class TestFizzBuzzTree
     {
-        [Fact]
-        public void CanFizzBuzzTree()
+        [Theory]
+        [InlineData("FizzBuzz", "45")]
+        [InlineData("Buzz", "5")]
+        [InlineData("Fizz", "9")]
+        public void CanFizzBuzzTree(string expected, string value)
         {
             //act
             BinaryTree binaryTree = new BinaryTree();
 
-            Node nodeA = new Node("15");
+            Node nodeA = new Node(value);
             Node nodeB = new Node("23");
             Node nodeC = new Node("57");
             Node nodeD = new Node("45");
@@ -27,9 +29,10 @@ namespace FizzBuzzTreeTest
             nodeC.LeftChild = nodeF;
 
             //arrange
-            Node found = FizzBuzzTree(nodeA);
+            Node found = binaryTree.FBTree(nodeA);
 
             //assert
+            Assert.Equal(expected, found.Value);
         }
     }
 }
