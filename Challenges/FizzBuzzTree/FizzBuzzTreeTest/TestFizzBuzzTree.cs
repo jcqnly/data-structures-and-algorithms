313fcc84e60ebf0b06b3ab5931b39cb7484b394d
jcqnly <jcqnly@gmail.com>
2018-06-27 09:47:12 -0700

    add test scaffolding

---
+++ Challenges/FizzBuzzTree/FizzBuzzTreeTest/TestFizzBuzzTree.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FizzBuzzTreeTest
+{
+    public class TestFizzBuzzTree
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
