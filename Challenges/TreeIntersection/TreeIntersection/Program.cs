b732a65c92be033dea3a3d6aa145a2ef123d4eac
jcqnly <jcqnly@gmail.com>
2018-07-17 22:50:52 -0700

    add references for tree intersection

--- Challenges/TreeIntersection/TreeIntersection/Program.cs
+++ Challenges/TreeIntersection/TreeIntersection/Program.cs
@@ -9,6 +9,17 @@ namespace TreeIntersection
             Console.WriteLine("Hello World!");
 
             //instantiate binary search tree 1
+            BinarySearchTree BST1 = new BinarySearchTree();
+
+            //declare nodes to add to BST1
+            //BSTNode nodeA = new BSTNode(100);
+            //BSTNode nodeB = new BSTNode(50);
+            //BSTNode nodeC = new BSTNode(150);
+            //BSTNode nodeD = new BSTNode(75);
+            //BSTNode nodeE = new BSTNode(300);
+
+            //BST1.Add();
+            //BST1.Add(nodeA, nodeB);
 
             //instantiate binary search tree 2
 
