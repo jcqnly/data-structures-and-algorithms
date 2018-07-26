64c00c7a7a11b1af6b3ae3a0b750346f5b6f5057
jcqnly <jcqnly@gmail.com>
2018-07-25 22:17:56 -0700

    add more comments to HashTable Left join

---
+++ Sorting_Algorithms/InsertionSort/InsertionSort/Classes/Sort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsertionSort.Classes
+{
+    public class Sort
+    {
+        public int[] InsertionSort(int[] arrayToSort)
+        {
+            int temp;
+            for (int i = 0; i < arrayToSort.Length; i++)
+            {
+                //current index is larger than the one to the right
+                if (arrayToSort[i] > arrayToSort[i + 1])
+                {
+                    temp = arrayToSort[i];
+                    arrayToSort[i + 1] = arrayToSort[i];
+                    arrayToSort[i + 1] = temp;
+                }
+
+            }
+            return arrayToSort;
+        }
+    }
+}
--- Sorting_Algorithms/InsertionSort/InsertionSort/Program.cs
+++ Sorting_Algorithms/InsertionSort/InsertionSort/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InsertionSort.Classes;
+using System;
 
 namespace InsertionSort
 {
@@ -7,6 +8,20 @@ namespace InsertionSort
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //create an array to work with
+            int[] unsortedArray = new int[] { 4, 10, 9, 2, 7, 6, 1, 5, 3, 8 };
+
+            //call insertion sort
+            Sort insertionSort = new Sort();
+
+            //pass the unsorted array to the InsertionSort method in the Sort class
+            int[] sortedArray = insertionSort.InsertionSort(unsortedArray);
+
+            //output sorted contents
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                Console.WriteLine($"{sortedArray[i]}");
+            }
         }
     }
 }
