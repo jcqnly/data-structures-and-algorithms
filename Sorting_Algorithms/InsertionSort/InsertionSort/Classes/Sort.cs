b8961a853260d55c340c5b9f310e5b5963aa92e1
jcqnly <jcqnly@gmail.com>
2018-07-29 17:45:31 -0700

    add tests

--- Sorting_Algorithms/InsertionSort/InsertionSort/Classes/Sort.cs
+++ Sorting_Algorithms/InsertionSort/InsertionSort/Classes/Sort.cs
@@ -6,20 +6,38 @@ namespace InsertionSort.Classes
 {
     public class Sort
     {
+        /// <summary>
+        /// Sort an unsorted array in ascending order.
+        /// With each iteration, one element from the given array
+        /// is inserted in place into the growing sorted list.
+        /// </summary>
+        /// <param name="arrayToSort"></param>
+        /// <returns>a sorted array in ascending order</returns>
         public int[] InsertionSort(int[] arrayToSort)
         {
             int i = 1;
+            //loop through the length of the array
             while (i < arrayToSort.Length)
             {
+                //store the 2nd value of the pair being analyzed
                 int x = arrayToSort[i];
+
+                //store the indexer of the  first number in the pair being analyzed
                 int j = i - 1;
+
+                //this loop will compare that 2nd value to every number in the 
+                //sorted array to the left of it
                 while (j >= 0 && arrayToSort[j] > x)
                 {
+                    //if it's larger than the number after it, swap the numbers
                     arrayToSort[j + 1] = arrayToSort[j];
-                    j = j - 1;
+                    //decrease the value of j, so that...
+                    j--;
                 }
+                //that spot can be replaced with the value that was replaced
                 arrayToSort[j + 1] = x;
-                i = i + 1;
+                //increase the indexer of where we are in the array
+                i++;
             }
             return arrayToSort;
         }
