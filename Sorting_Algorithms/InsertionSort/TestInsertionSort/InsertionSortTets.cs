d9207fa47753d7cd6eeb2f7176bbbe04340c5637
jcqnly <jcqnly@gmail.com>
2018-07-30 11:23:48 -0700

    change test conditions

--- Sorting_Algorithms/InsertionSort/TestInsertionSort/InsertionSortTets.cs
+++ Sorting_Algorithms/InsertionSort/TestInsertionSort/InsertionSortTets.cs
@@ -7,10 +7,10 @@ namespace TestInsertionSort
     public class InsertionSortTets
     {
         [Theory]
-        [InlineData(new int[] { 20, 4, 19, 21, 72, 16, 1, 51, 13, 800 }, 1)]
-        [InlineData(new int[] { 2, 9, 2, 7, 6, 12, 51, 13, 8 }, 2)]
-        [InlineData(new int[] { 20, 40, 109, 201, 702, 160, 100}, 20)]
-        public void CanSortArray(int[] value, int expected)
+        [InlineData(new int[] { 20, 4, 19, 21, 16, 1, 13 }, new int[] { 1, 4, 13, 16, 19, 20, 21})]
+        [InlineData(new int[] { 2, 9, 2, 7, 6, 12, 13, 8 }, new int[] { 2, 2, 6, 7, 8, 9, 12, 13})]
+        [InlineData(new int[] { 20, 40, 109, 201, 160, 100}, new int[] { 20, 40, 100, 109, 160, 201})]
+        public void CanSortArray(int[] value, int [] expected)
         {
             //arrange
             int[] unsortedArray = value;
@@ -21,7 +21,7 @@ namespace TestInsertionSort
             int[] sortedArray = insertionSort.InsertionSort(unsortedArray);
 
             //assert
-            Assert.Equal(expected, unsortedArray[0]);
+            Assert.Equal(expected, value);
         }
     }
 }
