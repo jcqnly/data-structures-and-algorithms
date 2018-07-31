d2eb99a7a55f7b3e9e551b747477a1ee5cc2cbaa
jcqnly <jcqnly@gmail.com>
2018-07-31 09:20:53 -0700

    add empty test solution

---
+++ Sorting_Algorithms/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,69 @@
+﻿using MergeSort.Classes;
+using System;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] unsortedArray = new int[] { 11, 4, 14, 7, 9, 1, 10, 12, 8, 15, 3, 6, 2, 13, 5};
+            MergeSort(unsortedArray);
+        }
+
+        public static void MergeSort(int[] unsortedArray)
+        {
+            MergeSort(unsortedArray, new int[unsortedArray.Length], 0, unsortedArray.Length-1);
+        }
+
+        public static void MergeSort(int[] arrayToSort, int[] temp, int leftStart, int rightEnd)
+        {
+            //base case
+            if (leftStart >= rightEnd) return;
+            else
+            {
+                int midIndex = (leftStart + rightEnd) / 2;
+                MergeSort(arrayToSort, temp, leftStart, midIndex);
+                MergeSort(arrayToSort, temp, midIndex + 1, rightEnd);
+                MergeHalves(arrayToSort, temp, leftStart, rightEnd);
+            }
+        }
+
+        public static void MergeHalves(int[] arrayToSort, int[] temp, int leftStart, int rightEnd)
+        {
+            int leftEnd = (leftStart + rightEnd) / 2;
+            int rightStart = leftEnd + 1;
+            int size = rightEnd - leftStart + 1;
+
+            int left = leftStart;
+            int right = rightStart;
+            int index = leftStart;
+
+            while (left <= leftEnd && right <= rightEnd)
+            {
+                if (arrayToSort[left] <= arrayToSort[right])
+                {
+                    temp[index] = arrayToSort[left];
+                    left++;
+                }
+                else
+                {
+                    temp[index] = arrayToSort[right];
+                    right++;
+                }
+                index++;
+            }
+
+            Array.Copy(arrayToSort, left, temp, index, leftEnd - left + 1);
+            Array.Copy(arrayToSort, right, temp, index, rightEnd - right + 1);
+            Array.Copy(temp, leftStart, arrayToSort, leftStart, size);
+
+            foreach (var num in arrayToSort)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("-----------");
+        }
+    }
+}
---
+++ Sorting_Algorithms/MergeSort/README.md
@@ -0,0 +1,28 @@
+# Merge Sort
+Given an array, sort it.
+
+---
+## Challenge
+Implement a merge sort
+
+---
+## Merge Sort Defined
+A merge sort is a recursive way of sorting a large array.  It is a 'divide and 
+conquer' style of sorting.  This style means that an array will be split in 
+half.  Those halves are split in half and so forth.  Those halves are sorted 
+and then those halves are sorted with the other half.
+
+---
+## Big O
+Since this breaks the array in two, the sort needs to do log n merges steps 
+because each merge step doubles the size of the array.  Since every number is 
+looked at, this algorithm runs in O(n log n) in terms of time.  In terms of 
+space, this will be O(1) because no new array is created to store the sorted 
+values.
+
+---
+## Visual
+![]()
+
+---
+## Problem Solving Process
