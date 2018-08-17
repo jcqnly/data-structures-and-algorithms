8537bf3948ff6ad3496ad671f1e6a0dda388b3f4
jcqnly <jcqnly@gmail.com>
2018-08-16 19:25:03 -0700

    add challenge PalindromePermutation

---
+++ Challenges/PalindromePermutation/PalindromePermutation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PalindromePermutation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string string1 = "civic";
+            string string2 = "ivicc";
+            string string3 = "llic";
+            Console.WriteLine(hasPalindromePermutation(string1));
+            Console.WriteLine(hasPalindromePermutation(string2));
+            Console.WriteLine(hasPalindromePermutation(string3));
+            
+        }
+
+        public static bool hasPalindromePermutation(string stringToTest)
+        {
+            var unpairedChar = new HashSet<char>();
+
+            foreach (char c in stringToTest)
+            {
+                if (unpairedChar.Contains(c))
+                {
+                    unpairedChar.Remove(c);
+                }
+                else
+                {
+                    unpairedChar.Add(c);
+                }
+            }
+
+            return unpairedChar.Count <= 1;
+        }
+    }
+}
