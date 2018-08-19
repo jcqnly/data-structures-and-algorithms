8015f979ad2fc1d19ccf109d84e91421cbf37dce
jcqnly <jcqnly@gmail.com>
2018-08-19 08:29:31 -0700

    add solution file

---
+++ Challenges/RotateArray/RotateArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RotateArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] {1, 2, 3 };
+            int rotations = 2;
+            Rotate(arr, rotations);
+        }
+
+        public static int[] Rotate(int[] arr, int rotations)
+        {
+
+            return arr;
+        }
+    }
+}
