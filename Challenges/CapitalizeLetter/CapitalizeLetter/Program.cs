e984131c41a64e3653720530f444215af9016df7
jcqnly <jcqnly@gmail.com>
2018-08-22 22:44:17 -0700

    add solution to CapitalizeLetter

---
+++ Challenges/CapitalizeLetter/CapitalizeLetter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CapitalizeLetter
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string stringToCapitalize = "my dog has fleas @pple";
+			CapitalizeLetterInString(stringToCapitalize);
+		}
+
+		public static void CapitalizeLetterInString(string stringToCapitalize)
+		{
+			char IsCapital = stringToCapitalize[0];
+			if (!ToUpper(stringToCapitalize[0]))
+			{
+
+			}
+			if (stringToCapitalize[0] != stringToCapitalize[0].ToUpper())
+			{
+
+			}
+		}
+	}
+}
