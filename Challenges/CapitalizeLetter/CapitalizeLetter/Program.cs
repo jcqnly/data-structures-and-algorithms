d40fb9c9b6a5cc568037578c3e83cbfd7c14f10c
jcqnly <jcqnly@gmail.com>
2018-08-23 08:39:02 -0700

    working solution

--- Challenges/CapitalizeLetter/CapitalizeLetter/Program.cs
+++ Challenges/CapitalizeLetter/CapitalizeLetter/Program.cs
@@ -6,21 +6,39 @@ namespace CapitalizeLetter
 	{
 		public static void Main(string[] args)
 		{
-			string stringToCapitalize = "my dog has fleas @pple";
+			string stringToCapitalize = "apple @pple aPPLES dog";
 			CapitalizeLetterInString(stringToCapitalize);
 		}
 
+		/// <summary>
+		/// Brute force way to capitalize the first letter of a word if it is
+		/// a letter in the alphabet
+		/// </summary>
+		/// <param name="stringToCapitalize"></param>
 		public static void CapitalizeLetterInString(string stringToCapitalize)
 		{
-			char IsCapital = stringToCapitalize[0];
-			if (!ToUpper(stringToCapitalize[0]))
+			string NewString = "";
+			if (!char.IsUpper(stringToCapitalize[0]))
 			{
-
+				NewString += char.ToUpper(stringToCapitalize[0]);
 			}
-			if (stringToCapitalize[0] != stringToCapitalize[0].ToUpper())
+
+			for (int i = 1; i < stringToCapitalize.Length; i++)
 			{
+				if (char.IsWhiteSpace(stringToCapitalize[i]))
+				{
+					NewString += stringToCapitalize[i];
+					i++;
 
+					if (char.IsLower(stringToCapitalize[i]))
+					{
+						NewString += char.ToUpper(stringToCapitalize[i]);
+						i++;
+					}
+				}
+				NewString += stringToCapitalize[i];
 			}
+			Console.WriteLine(NewString);
 		}
 	}
 }
