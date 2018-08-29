4a9124b1840ae8a51ef4b951ed46e1304d1d29fd
jcqnly <jcqnly@gmail.com>
2018-08-29 14:33:03 -0700

    add method skeleton to reverse words

--- Challenges/ReverseWordsInString/ReverseWordsInString/Program.cs
+++ Challenges/ReverseWordsInString/ReverseWordsInString/Program.cs
@@ -2,11 +2,25 @@
 
 namespace ReverseWordsInString
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+
+			string givenString = "Hello World";
+
+			//reverse words in a string
+			//Hello world -> world Hello
+
+			ReverseWords(givenString);
+		}
+
+		public static string ReverseWords(string givenString)
+		{
+
+
+			return givenString;
 		}
 	}
 }
