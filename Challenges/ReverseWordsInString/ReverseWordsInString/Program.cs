6f0b0be46be0a49329bc82239a4481b32de65ddd
jcqnly <jcqnly@gmail.com>
2018-08-30 14:19:55 -0700

    delete bruteforce strategy

--- Challenges/ReverseWordsInString/ReverseWordsInString/Program.cs
+++ Challenges/ReverseWordsInString/ReverseWordsInString/Program.cs
@@ -8,39 +8,16 @@ namespace ReverseWordsInString
 	{
 		public static void Main(string[] args)
 		{
-			//Console.WriteLine("Hello World!");
-
 			string givenString = "Hello world!";
 
 			//reverse words in a string
 			//Hello world -> world Hello
 
-			//ReverseWordsBruteForce(givenString);
-			Console.WriteLine(ReverseWordsStringBuilder(givenString));
-			
+			Console.WriteLine(ReverseWordsStringBuilder(givenString));		
 		}
 
 		/// <summary>
-		/// Brute force
-		/// </summary>
-		/// <param name="givenString"></param>
-		/// <returns></returns>
-		public static string ReverseWordsBruteForce(string givenString)
-		{
-			List<string> newString = new List<string>();
-			int lastSpace = -1;
-			for (int i = 0; i < givenString.Length; i++)
-			{
-				if (char.IsWhiteSpace(givenString[i]))
-				{
-					//newString.Add(lastSpace++, givenString[i]);
-				}
-			}
-			return givenString;
-		}
-
-		/// <summary>
-		/// Stringbuilder
+		/// Solving the problem with Stringbuilder
 		/// </summary>
 		/// <param name="givenString"></param>
 		/// <returns></returns>
