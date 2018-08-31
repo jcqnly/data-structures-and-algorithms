26bd45d3dc46db73b37d239734c8ca4df9386df9
jcqnly <jcqnly@gmail.com>
2018-08-31 13:13:18 -0700

    add tests for Capitalize Letter

--- Challenges/CapitalizeLetter/TestCapitalizeLetter/TestCapitalizeLetter.cs
+++ Challenges/CapitalizeLetter/TestCapitalizeLetter/TestCapitalizeLetter.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using static CapitalizeLetter.Program;
 
 namespace TestCapitalizeLetter
 {
 	public class TestCapitalizeLetter
 	{
-		[Fact]
-		public void Test1()
+		[Theory]
+		[InlineData("$loppy joe","$loppy Joe")]
+		[InlineData("4@ppy dog 1s BEST dOG", "4@ppy Dog 1s BEST DOG")]
+		[InlineData("1234", "1234")]
+		public void CanCapitalizeLetterWithStringBuilder(string input, string expected)
 		{
+			Assert.Equal(expected, CapitalizeLetterWithStringBuilder(input));
+		}
 
+		[Theory]
+		[InlineData("$loppy joe", "$loppy Joe")]
+		[InlineData("4@ppy dog 1s BEST dOG", "4@ppy Dog 1s BEST DOG")]
+		[InlineData("1234", "1234")]
+		public void CanCapitalizeLetterBruteForce(string input, string expected)
+		{
+			Assert.Equal(expected, CapitalizeLetterInString(input));
 		}
 	}
 }
