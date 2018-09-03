a4ce72d420c667244cdee08d5d18a3f7e39106f4
jcqnly <jcqnly@gmail.com>
2018-09-03 09:19:52 -0700

    add reduce letter solution files

---
+++ Challenges/ReduceLetters/ReduceLetters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReduceLetters
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			//given a string "aaabbc"
+			//reduce to "3a3b1c"
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
