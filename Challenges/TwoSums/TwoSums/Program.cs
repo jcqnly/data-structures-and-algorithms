c649bdcfc7ec076a503aea7fe79470da4f9328e3
jcqnly <jcqnly@gmail.com>
2018-09-26 12:00:14 -0700

    add modular TODO note

--- Challenges/TwoSums/TwoSums/Program.cs
+++ Challenges/TwoSums/TwoSums/Program.cs
@@ -2,11 +2,21 @@
 
 namespace TwoSums
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			int[] nums = new int[] { 2, 7, 11, 15};
+			int target = 9;
+			//TODO: modularize solution
+			//TwoSum(nums, target);
 		}
+
+		//public int[] TwoSum(int[] nums, int target)
+		//{
+
+		//	return 
+		//}
 	}
 }
