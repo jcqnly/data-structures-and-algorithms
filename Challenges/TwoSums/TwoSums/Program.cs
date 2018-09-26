2f1e543394b3fe6242e37e67cbd4e3804f9229cb
jcqnly <jcqnly@gmail.com>
2018-09-26 13:11:14 -0700

    solution found with n^2, double for loop

--- Challenges/TwoSums/TwoSums/Program.cs
+++ Challenges/TwoSums/TwoSums/Program.cs
@@ -6,13 +6,26 @@ namespace TwoSums
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//iterate through array
+			//if the number is greater than the target, ignore it and move on
+			//if the number is not greater than the target, store its index in a var
+				//iterate through the rest of the array
+				//if the number at each index, added to that first value, is the target
+				//store that index in a var
+
 			int[] nums = new int[] { 2, 7, 11, 15};
-			int target = 9;
+			int target = 13;
 
 			for (int i = 0; i < nums.Length; i++)
 			{
-
+				//int temp = nums[i];
+				for (int j = i+1; j < nums.Length; j++)
+				{
+					if (nums[i] + nums[j] == target)
+					{
+						Console.WriteLine($"The 2 indices are {i} and {j}");
+					}
+				}
 			}
 			//TODO: modularize solution
 			//TwoSum(nums, target);
