63d0729857f2cff0172db90826b81dc72307f42d
jcqnly <jcqnly@gmail.com>
2018-09-26 12:13:40 -0700

    add for loop

--- Challenges/TwoSums/TwoSums/Program.cs
+++ Challenges/TwoSums/TwoSums/Program.cs
@@ -9,6 +9,11 @@ namespace TwoSums
 			Console.WriteLine("Hello World!");
 			int[] nums = new int[] { 2, 7, 11, 15};
 			int target = 9;
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+
+			}
 			//TODO: modularize solution
 			//TwoSum(nums, target);
 		}
