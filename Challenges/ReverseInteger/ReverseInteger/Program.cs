495c70672bc1e1f8974de8ae0a83aaab77205f2e
jcqnly <jcqnly@gmail.com>
2018-10-01 19:32:27 -0700

    add stackoverflow answer

--- Challenges/ReverseInteger/ReverseInteger/Program.cs
+++ Challenges/ReverseInteger/ReverseInteger/Program.cs
@@ -9,26 +9,39 @@ namespace ReverseInteger
 		{
 			Console.WriteLine("Reverse Integer!");
 			int x = 122223;
-			Reverse(x);
+			int y = -1983;
+			//Reverse(x);
+			Console.WriteLine(Reverse(x));
+			Console.WriteLine(Reverse(y));
+			//Reverse(y);
 		}
 
-		public static int Reverse(int x)
+		public static int Reverse(int num)
 		{
+			//StackOverflow answer:
+			//int result = 0;
+			//while (num > 0)
+			//{
+			//	result = result * 10 + num % 10;
+			//	num /= 10;
+			//}
+			//return result;
 			int modulus;
 			List<int> reverseNum = new List<int>();
-			while (x >= 10)
+			while (num >= 10)
 			{
 				//1. find modulus of x
-				modulus = x % 10;
+				modulus = num % 10;
 				//2. add modulus to list
 				reverseNum.Add(modulus);
 				//3. divide x by 10 and store that in a diff var
-				x = x / 10;
+				num = num / 10;
 				//4. repeat 1-3 (using a while loop)
 			}
 			//add the last num to the list
-			reverseNum.Add(x);
-			return x;
+			reverseNum.Add(num);
+
+			return num;
 		}
 	}
 }
