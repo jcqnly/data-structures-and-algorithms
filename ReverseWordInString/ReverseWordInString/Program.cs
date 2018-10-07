2fc6e39b9aa06f8b684596dfb7e9bd5a578a32c9
jcqnly <jcqnly@gmail.com>
2018-10-06 21:20:49 -0700

    need to debug

--- ReverseWordInString/ReverseWordInString/Program.cs
+++ ReverseWordInString/ReverseWordInString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ReverseWordInString
 {
@@ -8,12 +9,25 @@ namespace ReverseWordInString
 		{
 			Console.WriteLine("Reverse Word in a String!");
 			string givenString = "Hello World!";
+			Console.WriteLine(ReverseWord(givenString));
 		}
 
 		public static string ReverseWord(string givenString)
 		{
-			
-			return givenString;
+			string[] splitString = givenString.Split(" ");
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < splitString.Length-1; i++)
+			{
+				for (int j = splitString[i].Length-1; j > 0; j--)
+				{
+					sb.Append(splitString[i][j]);
+				}
+				if (i != splitString.Length)
+				{
+					sb.Append(" ");
+				}
+			}
+			return sb.ToString();
 		}
 	}
 }
