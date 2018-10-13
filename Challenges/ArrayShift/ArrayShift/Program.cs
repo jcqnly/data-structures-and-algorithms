9413d436e98e658efdfdf14de1a60c8fd64691d7
jcqnly <jcqnly@gmail.com>
2018-10-13 16:56:37 -0700

    solution works.  need readme and linke on table of contents

---
+++ Challenges/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace ArrayShift
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			//can insert in an even array
+			int[] arr1 = { 1, 0, 6, 8 };
+
+			//can insert in an odd array
+			int[] arr2 = { 2, 1, 3, 8, 5 };
+
+			int midNum1 = 5;
+			int midNum2 = 7;
+
+			int[] result1 = Array_Shift(arr1, midNum1);
+			int[] result2 = Array_Shift(arr2, midNum2);
+		}
+
+		public static int[] Array_Shift(int[] arr, int midNum)
+		{
+			//make new array that will include extra digit
+			int[] newArr = new int[arr.Length + 1];
+
+			//find middle index of arr
+			decimal midIndex = Math.Ceiling((decimal)arr.Length/2);
+
+			//place midNum in middleIndex
+			for (int i = 0; i < newArr.Length; i++)
+			{
+				//if we're at the middle index, set that value to the number being inserted
+				if(i == midIndex) newArr[i] = midNum;
+				else if (i != midIndex && i > midIndex) newArr[i] = arr[i - 1];
+				else newArr[i] = arr[i];
+			}
+
+			foreach (int i in newArr) Console.Write(i);
+
+			Console.WriteLine("");
+			return newArr;
+		}
+
+		//public static void Main(string[] args)
+		//{
+		//	int[] array = { 2, 4, 6, 8, 10 };
+		//	int middleNum = 5;
+		//	int[] result = Array_Shift(array, middleNum);
+		//	string arrayAsString = "";
+		//	for (int i = 0; i < array.Length; i++)
+		//	{
+		//		arrayAsString = arrayAsString + array[i];
+		//	}
+		//	Console.WriteLine(arrayAsString);
+		//	Console.WriteLine(middleNum);
+		//	string resultString = "";
+
+		//	for (int i = 0; i < result.Length; i++)
+		//	{
+		//		resultString = resultString + result[i];
+		//	}
+		//	Console.WriteLine(resultString);
+		//}
+
+		//public static int[] Array_Shift(int[] array, int middleNum)
+		//{
+		//	int[] newArray = new int[array.Length + 1];
+		//	int counter = 0;
+		//	decimal midIndex = Math.Ceiling((decimal)array.Length / 2);
+		//	for (int i = 0; i < newArray.Length; i++)
+		//	{
+		//		if (midIndex == i)
+		//		{
+		//			newArray[i] = middleNum;
+		//		}
+		//		else
+		//		{
+		//			newArray[i] = array[counter++];
+		//		}
+		//	}
+		//	return newArray;
+		//}
+	}
+}
---
+++ Challenges/ArrayShift/ArrayShiftTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using static ArrayShift.Program;
+
+namespace ArrayShiftTest
+{
+	public class UnitTest1
+	{
+		//[Theory]
+		//[InlineData(9, new int[] { 8, 10, 9, 12, 1})]
+		//public void CanInsert(int value, int[] expected)
+		//{
+		//	//arrange
+		//	int[] arr1 = { 8, 10, 12, 1 };
+
+		//	//act
+		//	int[] result = Array_Shift(arr1, value);
+
+		//	//assert
+		//	Assert.Equal(result, expected);
+		//}
+	}
+}
