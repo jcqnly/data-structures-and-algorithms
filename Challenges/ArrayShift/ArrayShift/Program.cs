12501e61b329b58ce7a446f4c5da6cf16c4f579d
jcqnly <jcqnly@gmail.com>
2018-10-13 19:08:26 -0700

    add solution

--- Challenges/ArrayShift/ArrayShift/Program.cs
+++ Challenges/ArrayShift/ArrayShift/Program.cs
@@ -10,6 +10,7 @@ namespace ArrayShift
 			int[] arr1 = { 1, 0, 6, 8 };
 
 			//can insert in an odd array
+			//number to insert will be after the number that's currently the middle num
 			int[] arr2 = { 2, 1, 3, 8, 5 };
 
 			int midNum1 = 5;
@@ -19,6 +20,12 @@ namespace ArrayShift
 			int[] result2 = Array_Shift(arr2, midNum2);
 		}
 
+		/// <summary>
+		/// Insert a number in the middle of an array
+		/// </summary>
+		/// <param name="arr"></param>
+		/// <param name="midNum"></param>
+		/// <returns></returns>
 		public static int[] Array_Shift(int[] arr, int midNum)
 		{
 			//make new array that will include extra digit
@@ -32,54 +39,19 @@ namespace ArrayShift
 			{
 				//if we're at the middle index, set that value to the number being inserted
 				if(i == midIndex) newArr[i] = midNum;
+
+				//this is to account that the original array is shorter
+				//and to prevent and index out of range error because of the shorter length
 				else if (i != midIndex && i > midIndex) newArr[i] = arr[i - 1];
+
+				//otherwise, add the content of the original array to the new array
 				else newArr[i] = arr[i];
 			}
 
-			foreach (int i in newArr) Console.Write(i);
+			foreach (int i in newArr) Console.Write(i + " ");
 
 			Console.WriteLine("");
 			return newArr;
 		}
-
-		//public static void Main(string[] args)
-		//{
-		//	int[] array = { 2, 4, 6, 8, 10 };
-		//	int middleNum = 5;
-		//	int[] result = Array_Shift(array, middleNum);
-		//	string arrayAsString = "";
-		//	for (int i = 0; i < array.Length; i++)
-		//	{
-		//		arrayAsString = arrayAsString + array[i];
-		//	}
-		//	Console.WriteLine(arrayAsString);
-		//	Console.WriteLine(middleNum);
-		//	string resultString = "";
-
-		//	for (int i = 0; i < result.Length; i++)
-		//	{
-		//		resultString = resultString + result[i];
-		//	}
-		//	Console.WriteLine(resultString);
-		//}
-
-		//public static int[] Array_Shift(int[] array, int middleNum)
-		//{
-		//	int[] newArray = new int[array.Length + 1];
-		//	int counter = 0;
-		//	decimal midIndex = Math.Ceiling((decimal)array.Length / 2);
-		//	for (int i = 0; i < newArray.Length; i++)
-		//	{
-		//		if (midIndex == i)
-		//		{
-		//			newArray[i] = middleNum;
-		//		}
-		//		else
-		//		{
-		//			newArray[i] = array[counter++];
-		//		}
-		//	}
-		//	return newArray;
-		//}
 	}
 }
