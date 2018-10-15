57716622f503aca9f6358ea95b1765587f55b732
jcqnly <jcqnly@gmail.com>
2018-10-15 16:42:12 -0700

    needs better solution

--- Challenges/BalanceParans/BalanceParans/Program.cs
+++ Challenges/BalanceParans/BalanceParans/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BalanceParans
 {
@@ -7,23 +8,53 @@ namespace BalanceParans
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Balance paranthesis in a string");
+			string givenString1 = "g)a)v(u(r(r))";
+			Console.WriteLine($"Original string\n{givenString1}");
+			Console.WriteLine($"Balanced string\n{BalanceParenthesis(givenString1)}");
+			string givenString2 = "1+5)+5+)6+(5+9)*9";
+			Console.WriteLine($"Original string\n{givenString2}");
+			Console.WriteLine($"Balanced string\n{BalanceParenthesis(givenString2)}");
 
-			string givenString = "gua)ra)v(ku(mar(rajput))";
-
-			BalanceParenthesis(givenString);
 		}
 
 		public static string BalanceParenthesis(string givenString)
 		{
 			int counter = 0;
+
+			char[] parens = {'(', ')'};
+			StringBuilder sb = new StringBuilder();
 			for(int i = 0; i < givenString.Length; i++)
 			{
-				if()
+				//if char is open parens and there are no other open parens
+				if (givenString[i] == parens[0] && counter == 0)
 				{
-
+					//append it and incr counter
+					sb.Append(givenString[i]);
+					counter++;
 				}
+				else if (i == givenString.Length - 1 && counter > 0)
+				{
+					//append matching closing parens for as many open parens as there are
+					while (counter >= 0)
+					{
+						sb.Append(parens[1]);
+						counter--;
+					}
+				}
+				else if (givenString[i] == parens[1] && counter == 0)
+				{
+					//remove closing parens if we haven't come across an open parens yet
+					//do that by not appending anything
+					
+				}
+				else
+				{
+					sb.Append(givenString[i]);
+				}
+
 			}
-			return givenString;
+			
+			return sb.ToString();
 		}
 	}
 }
