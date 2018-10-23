2bd16a60cc5df74eac08ec9f705cb6da34794547
jcqnly <jcqnly@gmail.com>
2018-10-23 15:42:10 -0700

    add testing conditions for detect a capital

--- Challenges/DetectCapital/DetectCapital/Program.cs
+++ Challenges/DetectCapital/DetectCapital/Program.cs
@@ -6,7 +6,24 @@ namespace DetectCapital
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Detect Capital");
+			string string1 = "C"; //true
+			string string2 = "c"; //true
+			string string3 = "Dot Net Rocks!"; //true
+			string string4 = ".NET"; //true
+			string string5 = "BeE"; //false
+
+			Console.WriteLine($"{DetectCap(string1)}");
+			Console.WriteLine($"{DetectCap(string2)}");
+			Console.WriteLine($"{DetectCap(string3)}");
+			Console.WriteLine($"{DetectCap(string4)}");
+			Console.WriteLine($"{DetectCap(string5)}");
+		}
+
+		public static bool DetectCap(string input)
+		{
+
+			return true;
 		}
 	}
 }
