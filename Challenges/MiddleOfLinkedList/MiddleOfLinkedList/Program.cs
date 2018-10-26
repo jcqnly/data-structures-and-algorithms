08dde1ab6054587d27328662d9fc14fb471718a6
jcqnly <jcqnly@gmail.com>
2018-10-26 10:32:17 -0700

    add LinkedList class

--- Challenges/MiddleOfLinkedList/MiddleOfLinkedList/Program.cs
+++ Challenges/MiddleOfLinkedList/MiddleOfLinkedList/Program.cs
@@ -9,19 +9,11 @@ namespace MiddleOfLinkedList
 		{
 			Console.WriteLine("Find the Middle of a Linked List");
 			
-			LinkedList<Node> ll = new LinkedList<Node>();
-
-			ll.AddLast(new Node(3));
-			ll.AddLast(new Node(2));
-			ll.AddLast(new Node(1));
-			
-			FindMiddle(Node);
 		}
 
 		public static void FindMiddle(Node head)
 		{
 
-			Node current = head;
 		}
 	}
 }
