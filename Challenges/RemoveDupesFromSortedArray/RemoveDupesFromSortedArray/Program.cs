327a61dc19cb24e2a3d7480657bd15f6bc3bb620
jcqnly <jcqnly@gmail.com>
2018-11-01 22:01:39 -0700

    add array

--- Challenges/RemoveDupesFromSortedArray/RemoveDupesFromSortedArray/Program.cs
+++ Challenges/RemoveDupesFromSortedArray/RemoveDupesFromSortedArray/Program.cs
@@ -6,7 +6,20 @@ namespace RemoveDupesFromSortedArray
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Remove dupes from sorted array.");
+
+			int[] array = new int[] { 1, 1, 1, 2, 2, 3 };
+			RemoveDupes(array);
+		}
+
+		public static void RemoveDupes(int[] arr)
+		{
+			foreach (int i in arr)
+			{
+				Console.WriteLine(i);
+			}
+
+
 		}
 	}
 }
