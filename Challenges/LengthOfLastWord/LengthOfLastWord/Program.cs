2a580fdd7e3f0279efe9ef146c24825a224881df
jcqnly <jcqnly@gmail.com>
2018-11-05 22:39:39 -0700

    working solution for length of last word using a splti method

--- Challenges/LengthOfLastWord/LengthOfLastWord/Program.cs
+++ Challenges/LengthOfLastWord/LengthOfLastWord/Program.cs
@@ -6,7 +6,35 @@ namespace LengthOfLastWord
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Length of last word:");
+
+			//empty string
+			string input1 = "";
+			Console.WriteLine($"The string was '{input1}' and the length of the last word is: {LastWordLength(input1)}");
+
+			Console.WriteLine(" ----- ");
+
+			//regular string
+			string input2 = "Hello world";
+			Console.WriteLine($"The string was '{input2}' and the length of the last word is: {LastWordLength(input2)}");
+
+		}
+
+		public static int LastWordLength(string input)
+		{
+			//split words on space
+			string[] splitInput = input.Split(" ");
+
+			int counter = 0;
+			//iterate only through the last word
+			foreach (char i in splitInput[splitInput.Length-1])
+			{
+				//count the number of chars in the word
+				counter++;
+				Console.WriteLine(i);
+			}
+
+			return counter;
 		}
 	}
 }
