fc2adddd3ac7851bec7075e195db00b95c77495c
jcqnly <jcqnly@gmail.com>
2018-11-06 21:37:17 -0700

    start solutino file to tackle deleting a node

---
+++ Challenges/DeleteNode/DeleteNode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DeleteNode
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Delete a node");
+		}
+
+		//delete a node
+
+		//delete a node nth from the end
+	}
+}
