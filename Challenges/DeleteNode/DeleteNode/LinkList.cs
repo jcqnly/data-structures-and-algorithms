6c79ed97f53f3b0c5ad63506d8a81df1a65218e6
jcqnly <jcqnly@gmail.com>
2018-11-07 22:03:31 -0700

    start LinkList class

---
+++ Challenges/DeleteNode/DeleteNode/LinkList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeleteNode
+{
+	public class LinkList
+	{
+		public Node Head { get; set; }
+		public Node Current { get; set; }
+
+		public LinkList(Node node)
+		{
+			Head = node;
+			Current = node;
+		}
+
+		public void Add(Node node)
+		{
+			Current = Head;
+			node.Next = Head;
+			Head = node;
+			Current = Head;
+		}
+
+		public void AddBefore(Node newNode, Node existingNode)
+		{
+
+		}
+	}
+}
