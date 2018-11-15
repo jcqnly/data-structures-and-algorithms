21728f5dd9418c0c64efb876e8fda9627fc17871
jcqnly <jcqnly@gmail.com>
2018-11-14 21:21:28 -0700

    start challenge of find nums that add to a target number

---
+++ Challenges/AddToTargetNumber/AddToTargetNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AddToTargetNumber
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Find two numbers that add to a target number.");
+			int targetNumber = 10;
+			int[] arr = { 1, 8, 3, 5, 6 };
+			Console.WriteLine($"Target number is {targetNumber}.");
+
+		}
+
+		public static void FindNums()
+		{
+
+		}
+	}
+}
