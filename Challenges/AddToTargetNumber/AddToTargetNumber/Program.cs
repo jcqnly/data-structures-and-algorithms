d8408634cf05a6ad4837bf6645a178f3ee4f58c1
jcqnly <jcqnly@gmail.com>
2018-11-14 21:45:26 -0700

    finished challenge for finding target num with 2 nums

--- Challenges/AddToTargetNumber/AddToTargetNumber/Program.cs
+++ Challenges/AddToTargetNumber/AddToTargetNumber/Program.cs
@@ -7,15 +7,31 @@ namespace AddToTargetNumber
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Find two numbers that add to a target number.");
-			int targetNumber = 10;
-			int[] arr = { 1, 8, 3, 5, 6 };
-			Console.WriteLine($"Target number is {targetNumber}.");
+			int targetNum = 7;
+			int[] arr = { 1, 9, -2, 5, 6 };
+			Console.WriteLine($"Target number is {targetNum}.");
 
+			Console.WriteLine(FindTwoNums(arr, targetNum));
 		}
 
-		public static void FindNums()
+		public static bool FindTwoNums(int[] arr, int targetNum)
 		{
+			if (arr.Length < 2) return false;
 
+			for (int i = 0; i < arr.Length - 1; i++)
+			{
+				for (int j = 0; j < arr.Length - 1; j++)
+				{
+					if (i == j) continue;
+
+					if (arr[i] + arr[j] == targetNum)
+					{
+						return true;
+					}
+				}
+			}
+
+			return false;
 		}
 	}
 }
