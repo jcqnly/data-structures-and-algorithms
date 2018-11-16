55d9a8b4da7dddc496b833e0d41e8a112054b0cb
jcqnly <jcqnly@gmail.com>
2018-11-16 16:05:20 -0700

    return 3 nums that add to a target number

--- Challenges/AddToTargetNumber/AddToTargetNumber/Program.cs
+++ Challenges/AddToTargetNumber/AddToTargetNumber/Program.cs
@@ -7,18 +7,46 @@ namespace AddToTargetNumber
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Find two numbers that add to a target number and the array is:");
-			int targetNum = 6;
-			int[] arr = { 0, 3, -1, -2, 5, 7 };
-			foreach (int x in arr) Console.Write($"{x}  ");
-
-			Console.WriteLine($"Target number is {targetNum}.");
-			Console.WriteLine($"O(N^2) Solution returns ");
-			var numsList = FindTwoNums(arr, targetNum);
-			foreach (int i in numsList) Console.WriteLine(i);
+			//*Find 2 numbers that add to a target number.*
+			//Console.WriteLine("Find two numbers that add to a target number.  Return those 2" +
+			//	"numbers.  The array is:");
+			//int targetNum = 6;
+			//int[] arr = { 0, 1, -1, -2, 9, 3 };
+			//foreach (int x in arr) Console.Write($"{x}  ");
 
 			//Console.WriteLine($"Target number is {targetNum}.");
-			//Console.WriteLine($"O(N) Solution returns {FindTwoNumsFaster(arr, targetNum)}");
+			//Console.WriteLine($"O(N^2) Solution returns ");
+			//var numsList = FindTwoNums(arr, targetNum);
+			////if conditional is needed to check if the return value is null, so the proper msg
+			////or list of values can be displayed
+			//if (numsList == null)
+			//{
+			//	Console.WriteLine("No integers were found that added to the target.");
+			//}
+			//else
+			//{
+			//	foreach (int i in numsList) Console.WriteLine(i);
+			//}
+
+			////Console.WriteLine($"Target number is {targetNum}.");
+			////Console.WriteLine($"O(N) Solution returns {FindTwoNumsFaster(arr, targetNum)}");
+
+
+			//*Find 3 numbers that add to a target number*
+			Console.WriteLine("Find 3 numbers that add to a target number. Return those 3 numbers. " +
+				"The array is: ");
+			int targetNum = 6;
+			int[] arr = { 0, 1, -1, -2, 9, 3 };
+			foreach (int x in arr) Console.Write($"{x}  ");
+			var numsList = FindThreeNums(arr, targetNum);
+			if (numsList == null)
+			{
+				Console.WriteLine("No integers were found that added to the target.");
+			}
+			else
+			{
+				foreach (int i in numsList) Console.WriteLine(i);
+			}
 		}
 
 		/// <summary>
@@ -35,12 +63,9 @@ namespace AddToTargetNumber
 
 			for (int i = 0; i < arr.Length; i++)
 			{
-				for (int j = 0; j < arr.Length; j++)
+				//start at i + 1, so that i and j is not the same
+				for (int j = i+1; j < arr.Length; j++)
 				{
-					//this if statement is needed to make sure arr[i] and arr[j]
-					//doesn't start at the same index because we dont want the 
-					//index content to be added to itself
-					if (i == j) continue;
 					if (arr[i] + arr[j] == targetNum)
 					{
 						numsList.Add(arr[i]);
@@ -72,6 +97,33 @@ namespace AddToTargetNumber
 				numsList.Add(arr[i]);
 			}
 			return false;
+		}
+
+		public static List<int> FindThreeNums(int[] arr, int targetNum)
+		{
+			var numsList = new List<int>();
+			if (arr.Length < 3) return null;
+			for (int i  = 0; i < arr.Length; i++)
+			{
+				for (int j = i + 1; j < arr.Length; j++)
+				{
+					for (int k = j + 1; k < arr.Length; k++)
+					{
+						//if ((i == j) && (j == k)) continue;
+						if (arr[i] + arr[j] + arr[k] == targetNum)
+						{
+							numsList.Add(arr[i]);
+							Console.WriteLine($"arr[i] is {arr[i]}");
+							numsList.Add(arr[j]);
+							Console.WriteLine($"arr[j] is {arr[j]}");
+							numsList.Add(arr[k]);
+							Console.WriteLine($"arr[k] is {arr[k]}");
+							return numsList;
+						}
+					}
+				}
+			}
+			return null;
 		}
 	}
 }
