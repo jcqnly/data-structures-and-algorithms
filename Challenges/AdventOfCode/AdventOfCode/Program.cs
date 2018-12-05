905e4820a6fac313589bb8c30910041e0ee03a8e
jcqnly <jcqnly@gmail.com>
2018-12-05 10:24:48 -0700

    Finished 1st daf of Advent of Code

--- Challenges/AdventOfCode/AdventOfCode/Program.cs
+++ Challenges/AdventOfCode/AdventOfCode/Program.cs
@@ -10,6 +10,7 @@ namespace AdventOfCode
 		{
 			Console.WriteLine("Advent of Code");
 
+			//list to store numbers that will be read in using StreamReader
 			List<int> numList = new List<int>();
 			string path = "../../../numbers.txt";
 
@@ -18,17 +19,27 @@ namespace AdventOfCode
 				string s = "";
 				while ((s = sr.ReadLine()) != null)
 				{
+					//read each line as an int
 					int num = Int32.Parse(s);
 					numList.Add(num);
-					Console.WriteLine(num);
 				}
 			}
-
+			Console.WriteLine($"Frequency is {FrequencyDay1(numList)}.");
 		}
 
-		public static int FrequencyDay1()
+		/// <summary>
+		/// Takes in the list of nums and adds the values to a sum
+		/// </summary>
+		/// <param name="numList"></param>
+		/// <returns>integer that represents the frequency</returns>
+		public static int FrequencyDay1(List<int> numList)
 		{
-
+			int sum = 0;
+			for (int i = 0; i < numList.Count; i++)
+			{
+				sum += numList[i];
+			}
+			return sum;
 		}
 	}
 }
