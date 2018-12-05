206094e01528976e374cb56b909fc5cdd2415511
jcqnly <jcqnly@gmail.com>
2018-12-04 18:57:02 -0700

    streamreader works and can read in a text file full of the numbers given for the problem domain

---
+++ Challenges/AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Advent of Code");
+
+			List<int> numList = new List<int>();
+			string path = "../../../numbers.txt";
+
+			using (StreamReader sr = File.OpenText(path))
+			{
+				string s = "";
+				while ((s = sr.ReadLine()) != null)
+				{
+					int num = Int32.Parse(s);
+					numList.Add(num);
+					Console.WriteLine(num);
+				}
+			}
+
+		}
+
+		public static int FrequencyDay1()
+		{
+
+		}
+	}
+}
