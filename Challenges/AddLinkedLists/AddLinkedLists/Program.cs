359b19037ce5a833a2d11e8ef7d2a92610a1f18b
jcqnly <jcqnly@gmail.com>
2018-12-12 12:27:37 -0700

    separated summation into its own method

--- Challenges/AddLinkedLists/AddLinkedLists/Program.cs
+++ Challenges/AddLinkedLists/AddLinkedLists/Program.cs
@@ -33,24 +33,50 @@ namespace AddLinkedLists
 
 			int carryOver = 0;
 
-			while (currA != null)
+			//edge case: both LLs are empty
+			if (currA == null && currB == null)
 			{
-				int sum = currA.Value.Value + currB.Value.Value + carryOver;
-				if (sum >= 10) carryOver = 1;
-				//get the num in the ones place
-				int remainder = sum % 10;
-				LLC.AddLast(new Node(remainder));
+				return null;
+			}
 
+			while (currA != null || currB != null)
+			{
+				if (SumNodes(LLC, currA, currB, carryOver) == 1) carryOver = 1;
+				else carryOver = 0;
+				//move along the LL
 				currA = currA.Next;
 				currB = currB.Next;
+
+				//edge case: if both nodes next in both lists are null and there's a carryOver
+				//create an extra new node
+				if (currA == null && currB == null && carryOver == 1)
+				{
+					LLC.AddLast(new Node(carryOver));
+				}
 			}
 
 			Print(LLC.First);
 			return LLC.First;
 		}
 
+		public static int SumNodes(LinkedList<Node>LL, LinkedListNode<Node> A, LinkedListNode<Node> B, int carryOver)
+		{
+			int sum = A.Value.Value + B.Value.Value + carryOver;
+			int remainder = sum % 10; //get the num in the ones place
+			LL.AddLast(new Node(remainder)); //create a new node
+			//get the num to carryOver
+			if (sum >= 10) return carryOver = 1;
+			return carryOver = 0;
+		}
+
+		/// <summary>
+		/// print the LL
+		/// </summary>
+		/// <param name="LLNode"></param>
 		public static void Print(LinkedListNode<Node> LLNode)
 		{
+			Console.WriteLine();
+			Console.WriteLine("---");
 			while (LLNode != null)
 			{
 				Console.Write($"  {LLNode.Value.Value}  ");
