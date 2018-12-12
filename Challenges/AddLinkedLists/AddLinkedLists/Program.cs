8009dc51f8fd48574dda051f03ac72c0ab46dbcb
jcqnly <jcqnly@gmail.com>
2018-12-11 21:49:05 -0700

    start add Linked List challenge

---
+++ Challenges/AddLinkedLists/AddLinkedLists/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddLinkedLists
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Add values in a linked list and return a new list");
+			//declare LL1 and LL2
+
+			//call method with the heads of LL1 and LL2 passed in
+		}
+	}
+}
