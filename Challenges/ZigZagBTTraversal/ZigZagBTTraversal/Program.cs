a00ce483b2296a42aae0b9e2ee4d81a8955508af
jcqnly <jcqnly@gmail.com>
2018-12-13 16:16:15 -0700

    start solution file for zig zag traversal

---
+++ Challenges/ZigZagBTTraversal/ZigZagBTTraversal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZigZagBTTraversal
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Zig zag traversal of a binary tree");
+
+			//create a BT
+
+		}
+
+		
+	}
+}
