08b431c85d68d337a4f6eec7da84b009ad4a78b5
jcqnly <jcqnly@gmail.com>
2018-12-15 15:57:26 -0700

    add my solution to repeatedly add nums together

--- Challenges/AddDigits/AddDigits/Program.cs
+++ Challenges/AddDigits/AddDigits/Program.cs
@@ -2,11 +2,32 @@
 
 namespace AddDigits
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Repeatedly add digits in a number until 1 remains:");
+
+			int num = 49;
+
+			Console.WriteLine(AddDigitsTogether(num));
+		}
+
+		public static int AddDigitsTogether(int num)
+		{
+			int sum = num;
+
+			while (sum > 9)
+			{
+				//modulus the number to get to the ones place
+				int onesPlace = sum % 10;
+
+				//modulus the number to get to the ten place
+				int tensPlace = sum /10 % 10;
+
+				sum = onesPlace + tensPlace;
+			}
+			return sum;
 		}
 	}
 }
