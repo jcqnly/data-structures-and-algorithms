41a7d1f0c68742d78ed8e23fc1ad4fc15e07b1f2
jcqnly <jcqnly@gmail.com>
2018-12-15 18:04:07 -0700

    add Recursive method, but it doesnt add the last 2 numbers.  add comments to the iterative solution that can repeatedly sum any number

--- Challenges/AddDigits/AddDigits/Program.cs
+++ Challenges/AddDigits/AddDigits/Program.cs
@@ -8,11 +8,22 @@ namespace AddDigits
 		{
 			Console.WriteLine("Repeatedly add digits in a number until 1 remains:");
 
-			int num = 49;
-
+			int num = 38;
 			Console.WriteLine(AddDigitsTogether(num));
+			Console.WriteLine(AddDigitsRecursively(num));
+
+			int num2 = 12345;
+			Console.WriteLine(AddAnyNumberOfDigits(num2));
+			Console.WriteLine(AddDigitsRecursively(num2));
 		}
 
+
+		/// <summary>
+		/// Non-recursive, iterative solution
+		/// Repeatedly sums all the digits is a number
+		/// </summary>
+		/// <param name="num"></param>
+		/// <returns>sum of all the digits</returns>
 		public static int AddDigitsTogether(int num)
 		{
 			int sum = num;
@@ -28,6 +39,40 @@ namespace AddDigits
 				sum = onesPlace + tensPlace;
 			}
 			return sum;
+		}
+
+		//recursive method currently doesn't add the final 2 numbers
+		public static int AddDigitsRecursively(int num)
+		{
+			if (num > 0)
+			{
+				return num % 10 + AddDigitsRecursively(num / 10);
+			}
+
+			return num % 10 + (num/10);
+		}
+
+		//not my solution, but comments explain how I read this
+		public static int AddAnyNumberOfDigits(int num)
+		{
+			//x will keep the sum of each digit
+			int x = 0;
+			while (num != 0)
+			{
+				//add the value of the ones place of num to x
+				x += num % 10;
+				//divide num by 10 to get the tens place
+				num /= 10;
+
+				//if num is 0 and the sum is still more than 2 digits
+				if (num == 0 && x >= 10)
+				{
+					//set num to the value of x to sum the final 2 numbers
+					num = x;
+					x = 0;
+				}
+			}
+			return x;
 		}
 	}
 }
--- Challenges/AddDigits/README.md
+++ Challenges/AddDigits/README.md
@@ -8,3 +8,14 @@ one remain.
 
 ### Output
 Calculation should be: 4 + 9 -> 1 + 3 -> 4
+
+## Problem Solving Process
+
+1. The non-recursive solution cannot handle 
+more than 2 digits.
+
+2. Recursive solution is a work in progress.
+
+## Acknowledgements
+
+The math explanation is found [here](https://math.stackexchange.com/questions/654523/repeatedly-summing-the-digits-of-a-number?rq=1).
