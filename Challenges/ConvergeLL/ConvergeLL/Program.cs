2199a84efe90ee59123437c885ddaa6881d571db
jcqnly <jcqnly@gmail.com>
2018-12-16 10:49:59 -0700

    start convergeLL challenge

---
+++ Challenges/ConvergeLL/ConvergeLL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConvergeLL
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Determine if 2 LLs converge.  If they do, find that conversion point.");
+		}
+	}
+}
