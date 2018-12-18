51e46f8f229c1b41984c7ab8978def88fb498e6e
jcqnly <jcqnly@gmail.com>
2018-12-18 12:09:49 -0700

    add BST class

--- Challenges/ValidateBST/ValidateBST/Program.cs
+++ Challenges/ValidateBST/ValidateBST/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ValidateBST.Classes;
 
 namespace ValidateBST
 {
@@ -7,9 +8,36 @@ namespace ValidateBST
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Validate a BST");
+
+			BST bst = new BST();
+
+			Node nodeA = new Node(10);
+			Node nodeB = new Node(5);
+			Node nodeC = new Node(15);
+			Node nodeD = new Node(2);
+			Node nodeE = new Node(7);
+			Node nodeF = new Node(30);
+
+			bst.Add(nodeA, nodeB);
+			bst.Add(nodeA, nodeC);
+			bst.Add(nodeA, nodeD);
+			bst.Add(nodeA, nodeE);
+			bst.Add(nodeA, nodeF);
+
+			InOrder(nodeA);
 		}
 
+		public static void InOrder(Node node)
+		{
+			if (node.Left != null) InOrder(node.Left);
+			Console.WriteLine($"{node.Value}");
+			if (node.Right != null) InOrder(node.Right);
+		}
 
+		public bool Validate(Node node)
+		{			
 
+			return true;
+		}
 	}
 }
