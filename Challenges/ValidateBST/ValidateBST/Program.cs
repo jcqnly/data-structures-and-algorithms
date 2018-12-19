15f1f4d8daf2c4962233dd1373cfee1d711a2515
jcqnly <jcqnly@gmail.com>
2018-12-18 19:35:07 -0700

    finish InOrder traversal to validate a BST

--- Challenges/ValidateBST/ValidateBST/Program.cs
+++ Challenges/ValidateBST/ValidateBST/Program.cs
@@ -24,14 +24,49 @@ namespace ValidateBST
 			bst.Add(nodeA, nodeE);
 			bst.Add(nodeA, nodeF);
 
-			InOrder(nodeA);
+			//InOrder(nodeA);
+			Console.WriteLine(InOrder(nodeA));
+			Node node1 = new Node(2);
+			Node node2 = new Node(7);
+			Node node3 = new Node(5);
+			Node node4 = new Node(2);
+			Node node5 = new Node(6);
+			Node node6 = new Node(5);
+			Node nodeG = new Node(11);
+			Node nodeH = new Node(9);
+			Node nodeI = new Node(5);
+
+			//setting the values of left and right of each node
+			node1.Left = nodeB;
+			node1.Right = nodeC;
+			node2.Left = nodeD;
+			node2.Right = nodeE;
+			node5.Left = nodeF;
+			node5.Right = nodeG;
+			nodeF.Right = nodeI;
+			nodeC.Right = nodeF;
+
+			Console.WriteLine(InOrder(node1));
 		}
 
-		public static void InOrder(Node node)
+		//Validate using InOrder Traversal
+		public static bool InOrder(Node node)
 		{
-			if (node.Left != null) InOrder(node.Left);
-			Console.WriteLine($"{node.Value}");
-			if (node.Right != null) InOrder(node.Right);
+			if (node == null) return true;
+
+			if (node.Left != null)
+			{
+				if (node.Left.Value > node.Value) return false;
+				InOrder(node.Left);
+			}
+
+			if (node.Right != null)
+			{
+				if (node.Right.Value < node.Value) return false;
+				InOrder(node.Right);
+			}
+
+			return true;
 		}
 
 		public bool Validate(Node node)
