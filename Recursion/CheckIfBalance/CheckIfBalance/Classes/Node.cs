a5ee6080274d346bef9b45706c254a30d781ff18
jcqnly <jcqnly@gmail.com>
2018-12-22 17:16:47 -0700

    add base cases to recursive methods

--- Recursion/CheckIfBalance/CheckIfBalance/Classes/Node.cs
+++ Recursion/CheckIfBalance/CheckIfBalance/Classes/Node.cs
@@ -7,7 +7,8 @@ namespace CheckIfBalance.Classes
 	public class Node
 	{
 		public int Value { get; set; }
-		public Node Next { get; set; }
+		public Node Left { get; set; }
+		public Node Right { get; set; }
 
 		public Node(int value)
 		{
--- Recursion/CheckIfBalance/CheckIfBalance/Program.cs
+++ Recursion/CheckIfBalance/CheckIfBalance/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CheckIfBalance.Classes;
+using System;
 
 namespace CheckIfBalance
 {
@@ -8,9 +9,37 @@ namespace CheckIfBalance
 		{
 			Console.WriteLine("Hello World!");
 
+			//create tree
+			Node nodeA = new Node(1);
+			Node nodeB = new Node(2);
+			Node nodeC = new Node(3);
+			Node nodeD = new Node(4);
+			Node nodeE = new Node(5);
 
+			nodeA.Left = nodeB;
+			nodeA.Right = nodeC;
+			nodeC.Left = nodeD;
+			nodeD.Right = nodeE;
+
+
+			Console.WriteLine(CheckBalance(nodeA));
 		}
 
+		public static bool CheckBalance(Node node)
+		{
+			return Max(node) - Min(node) <= 1;
+		}
 
+		public static int Min(Node node)
+		{
+			if (node == null) return 0;
+			return 1 + Math.Min(Min(node.Left), Min(node.Right));
+		}
+
+		public static int Max(Node node)
+		{
+			if (node == null) return 0; 
+			return 1 + Math.Max(Max(node.Left), Max(node.Right));
+		}
 	}
 }
