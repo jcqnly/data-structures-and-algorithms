7dc488db6e26d5e2d668bcbb288855f4b1bfa90d
jcqnly <jcqnly@gmail.com>
2018-12-23 23:56:56 -0700

    start Min BST challenge

---
+++ Challenges/MinBST/MinBST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinBST
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Create a BST with the least amount of layers from a sorted array.");
+		}
+	}
+}
---
+++ Challenges/MinBST/README.md
@@ -0,0 +1,7 @@
+# Min BST
+
+A challenge from Cracking the Coding Interview book.
+
+## Problem Domain
+Given a sorted array of unique ints, create a BST with the least amount 
+of layers.
