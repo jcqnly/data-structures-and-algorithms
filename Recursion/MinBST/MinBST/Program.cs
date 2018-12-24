58e491ef92fd5b4396a2d5fe5027345ecb938cff
jcqnly <jcqnly@gmail.com>
2018-12-24 11:50:25 -0700

    start working on method to recursively make Nodes

--- Recursion/MinBST/MinBST/Program.cs
+++ Recursion/MinBST/MinBST/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MinBST.Classes;
+using System;
 
 namespace MinBST
 {
@@ -7,6 +8,20 @@ namespace MinBST
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Create a BST with the least amount of layers from a sorted array.");
+			int[] arr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8};
+			NewBST(arr, 0, arr.Length - 1);
+		}
+
+		public static Node NewBST(int[] arr, int start, int end)
+		{
+			while (start < end)
+			{
+				int mid = (start + end) / 2;
+				Node root = new Node(arr[mid]);
+
+				root.Left = NewBST(arr, 0, mid - 1);
+				root.Right = NewBST(arr, mid + 1, end);
+			}
 		}
 	}
 }
