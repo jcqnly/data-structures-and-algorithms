34fe968f0a7ac0080bbd9ac4fad6927639350cd3
jcqnly <jcqnly@gmail.com>
2018-12-29 14:15:21 -0700

    start StackMin project

---
+++ Challenges/StackMin/README.md
@@ -0,0 +1,6 @@
+# Stack Min
+Write a method that returns the min element of a stack.  Push, pop, and 
+min should operate in O(1) time.
+
+This problem is from Cracking the Coding Interview.
+
---
+++ Challenges/StackMin/StackMin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackMin
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Return the minimum element of a stack.");
+		}
+	}
+}
