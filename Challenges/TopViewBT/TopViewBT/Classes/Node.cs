cdc43819b8d3e1c3b5a7fca7d69f309fee8eecf2
jcqnly <jcqnly@gmail.com>
2019-01-02 20:10:28 -0700

    add node class

---
+++ Challenges/TopViewBT/TopViewBT/Classes/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopViewBT.Classes
+{
+	public class Node
+	{
+		public Node Left { get; set; }
+		public Node Right { get; set; }
+		public int Value { get; set; }
+
+		public Node(int value)
+		{
+			Value = value;
+		}
+	}
+}
