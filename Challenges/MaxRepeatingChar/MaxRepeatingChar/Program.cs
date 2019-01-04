57047353a0a892b267ccfe33ed2a9591206cf18d
jcqnly <jcqnly@gmail.com>
2019-01-03 20:51:22 -0700

    start max consecutive repeating char

---
+++ Challenges/MaxRepeatingChar/MaxRepeatingChar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaxRepeatingChar
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Find Maximum Consecutive Repeating Char in a String");
+		}
+	}
+}
