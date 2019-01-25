da5961284b8dc2894bcd21562d2e1c6788e2c67b
jcqnly <jcqnly@gmail.com>
2019-01-25 15:42:20 -0700

    start uniVal challenge

---
+++ Recursion/UniVal/README.md
@@ -0,0 +1,10 @@
+# Univalue BT
+
+Check if all the nodes in a BT are all the same values.
+
+Problem from [here](https://leetcode.com/problems/univalued-binary-tree/)
+
+## Why
+This is an exercise in understanding how recursion works.  While 
+traveling to each node, the value of the current node will be compared 
+with the one after it, if it exists.
---
+++ Recursion/UniVal/UniVal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UniVal
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Determine if all the nodes in a BT have the same value");
+			Node nodeA = new Node(1);
+			Node nodeB = new Node(1);
+			Node nodeC = new Node(1);
+			Node nodeD = new Node(1);
+			Node nodeE = new Node(1);
+			Node nodeF = new Node(1);
+
+			nodeA.Left = nodeB;
+			nodeA.Right = nodeC;
+
+			nodeB.Left = nodeD;
+			nodeB.Right = nodeE;
+
+			nodeC.Right = nodeF;
+		}
+
+		public static bool IsUnival()
+		{
+
+		}
+	}
+}
