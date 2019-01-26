db068ddb86eba7155c0471b0765b0094471421d5
jcqnly <jcqnly@gmail.com>
2019-01-26 09:04:24 -0700

    add explanation to solution

--- Recursion/UniVal/README.md
+++ Recursion/UniVal/README.md
@@ -4,7 +4,12 @@ Check if all the nodes in a BT are all the same values.
 
 Problem from [here](https://leetcode.com/problems/univalued-binary-tree/)
 
-## Why
-This is an exercise in understanding how recursion works.  While 
-traveling to each node, the value of the current node will be compared 
-with the one after it, if it exists.
+This is an exercise in understanding how recursion works.  
+
+## How This Recursion Solution Works
+The base case for this traversal is when a node has no child nodes.  
+That node is taken of the call stack and we are back to the node before 
+it, its parent node.  The parent node's value is checked against its 
+child node, the previous node; at any point where the parent's node value
+is not the same as its child node's, then the method breaks and a false 
+is returned
--- Recursion/UniVal/UniVal/Program.cs
+++ Recursion/UniVal/UniVal/Program.cs
@@ -13,6 +13,7 @@ namespace UniVal
 			Node nodeD = new Node(1);
 			Node nodeE = new Node(1);
 			Node nodeF = new Node(1);
+			Node nodeG = new Node(2);
 
 			nodeA.Left = nodeB;
 			nodeA.Right = nodeC;
@@ -21,11 +22,32 @@ namespace UniVal
 			nodeB.Right = nodeE;
 
 			nodeC.Right = nodeF;
+			nodeC.Left = nodeG;
+			bool answer = IsUnival(nodeA);
+			Console.WriteLine(answer);
 		}
 
-		public static bool IsUnival()
+		/// <summary>
+		/// Recursive method that only checks the value of the current 
+		/// node against the value of its children's nodes
+		/// </summary>
+		/// <param name="root">starting node</param>
+		/// <returns>boolean value</returns>
+		public static bool IsUnival(Node root)
 		{
+			if (root.Left != null)
+			{
+				if (!IsUnival(root.Left)) return false;
+				if (root.Left.Value != root.Value) return false;
+			}
 
+			if (root.Right != null)
+			{
+				if (!IsUnival(root.Right)) return false;
+				if (root.Right.Value != root.Value) return false;
+			}
+
+			return true;
 		}
 	}
 }
