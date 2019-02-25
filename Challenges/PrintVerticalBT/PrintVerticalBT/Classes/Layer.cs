598b373fa2e50ec175a2c3bfc7f735cafa3161ca
jcqnly <jcqnly@gmail.com>
2019-02-25 12:44:48 -0700

    add while loop to chug through Q

---
+++ Challenges/PrintVerticalBT/PrintVerticalBT/Classes/Layer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintVerticalBT.Classes
+{
+	public class Layer
+	{
+		public int HD { get; set; }
+		public Node Root { get; set; }
+
+		public Layer(int hd, Node root)
+		{
+			HD = hd;
+			Root = root;
+		}
+	}
+}
--- Challenges/PrintVerticalBT/PrintVerticalBT/Program.cs
+++ Challenges/PrintVerticalBT/PrintVerticalBT/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PrintVerticalBT.Classes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace PrintVerticalBT
 {
@@ -7,6 +10,60 @@ namespace PrintVerticalBT
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Vertically print a BT");
+
+			//make the tree
+			Node node1 = new Node(1);
+			Node node2 = new Node(2);
+			Node node3 = new Node(3);
+			Node node4 = new Node(4);
+			Node node5 = new Node(5);
+			Node node6 = new Node(6);
+
+			node1.Left = node2;
+			node1.Right = node3;
+			node2.Right = node4;
+			node4.Right = node5;
+			node5.Right = node6;
+
+
+		}
+
+		public static void Vertical(Node root, int level)
+		{
+			if (root == null) return;
+			Queue<Layer> q = new Queue<Layer>();
+
+			var vertical = new Dictionary<int, List<Node>>();
+
+			q.Enqueue(new Layer(0, root));
+
+			while (q.Count > 0)
+			{
+				Layer temp = q.Dequeue();
+				Node tnode = temp.Root;
+				int HD = temp.HD;
+
+				//if the dictionary has that key, add the other nodes found at that HD
+				if (vertical.ContainsKey(HD))
+				{
+					vertical[HD].Add(root);
+				}
+				else
+				{
+					vertical.Add(HD, new List<Node> { root });
+				}
+
+				if (root.Left != null) 
+				{
+					q.Enqueue(new Layer(HD - 1, root.Left));
+				}
+
+				if (root.Right != null)
+				{
+					q.Enqueue(new Layer(HD + 1, root.Right));
+				}
+			}
+
 		}
 	}
 }
